02666b852554e9863dba45dda66bfdf9f4fe1ea2
Teri Chudějová <tereza.chudejova@gmail.com>
2024-08-22 00:58:45 +0200

    make wavy background, variables, links

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id='mainContainer'>
+
+
+      {/* HEADER */}
+      <div className='header outerBox'>
+        <div className='innerBox'>
+          <ul>
+            <li><a>O mně</a></li>
+            <li><a>Časová osa</a></li>
+            <li><a>Dovednosti</a></li>
+            <li><a>Má práce</a></li>
+            <li><a>Kontaktuj mě</a></li>
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+
+      {/* FIRST SECTION - LANDING PAGE */}
+      <div className='firstSection-landingPage outerBox'>
+        <div className='innerBox'>
+          <h1>Teri</h1>
+          <h2>Front-End Developerka</h2>
+          <p>Jmenuji se Tereza Chudějová a jsem front-end developerka s IT základy získanými během studia na vysoké škole a vášní pro vytváření moderních, uživatelsky přívětivých webových stránek. Mám bakalářský titul z Aplikované informatiky a inženýrský titul z Informačního managementu, což mi poskytuje technické znalosti a schopnost propojit různé aspekty vývoje, designu a firemních potřeb. Po 5 letech práce jako datový analytik jsem se rozhodla následovat svou vášeň pro kreativní řešení a začala se intenzivně věnovat front-end vývoji, kde mohu spojit logické myšlení s estetikou a designem.</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+      {/* SECTION - BLUE WAVE */}
+      <div className='blueWave outerBox'>
+
+      </div>
+
+
+      {/* SECOND SECTION */}
+      <div className="secondSection outerBox">
+
+      </div>
+
+
+      <div className="footer outerBox">
+
+      </div>
+
+
+    </div>
   )
 }
 
