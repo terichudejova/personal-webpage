21eaa60f3c94e841b6baae470d68c2ea99fa25c1
Teri Chudějová <tereza.chudejova@gmail.com>
2024-08-24 00:07:40 +0200

    add correct wave, add profile image, change colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
 
   return (
@@ -9,32 +10,35 @@ function App() {
 
       {/* HEADER */}
       <div className='header outerBox'>
+        <div className="gradient-bg"></div>
+          <div className="innerBox">
+            <div className="links">
+              <ul>
+                <li><a>O mně</a></li>
+                <li><a>Časová osa</a></li>
+                <li><a>Dovednosti</a></li>
+                <li><a>Má práce</a></li>
+                <li><a>Kontaktuj mě</a></li>
+              </ul>
+            </div>
+            <div className="about">
+              <h1>Ahoj, jsem Teri.</h1>
+              <h2>Front-End Developer</h2>
+              <div className='profileImage'>
+                <img id='profileImage' src='images/profile.png'/>
+              </div>
+            </div>
+          </div>
+      </div>
+
+
+
+      {/* FIRST SECTION - ABOUT ME */}
+      <div className='firstSection-aboutMe outerBox'>
         <div className='innerBox'>
-          <ul>
-            <li><a>O mně</a></li>
-            <li><a>Časová osa</a></li>
-            <li><a>Dovednosti</a></li>
-            <li><a>Má práce</a></li>
-            <li><a>Kontaktuj mě</a></li>
-          </ul>
         </div>
       </div>
 
-
-      {/* FIRST SECTION - LANDING PAGE */}
-      <div className='firstSection-landingPage outerBox'>
-        <div className='innerBox'>
-          <h1>Teri</h1>
-          <h2>Front-End Developerka</h2>
-          <p>Jmenuji se Tereza Chudějová a jsem front-end developerka s IT základy získanými během studia na vysoké škole a vášní pro vytváření moderních, uživatelsky přívětivých webových stránek. Mám bakalářský titul z Aplikované informatiky a inženýrský titul z Informačního managementu, což mi poskytuje technické znalosti a schopnost propojit různé aspekty vývoje, designu a firemních potřeb. Po 5 letech práce jako datový analytik jsem se rozhodla následovat svou vášeň pro kreativní řešení a začala se intenzivně věnovat front-end vývoji, kde mohu spojit logické myšlení s estetikou a designem.</p>
-        </div>
-      </div>
-
-
-      {/* SECTION - BLUE WAVE */}
-      <div className='blueWave outerBox'>
-
-      </div>
 
 
       {/* SECOND SECTION */}
