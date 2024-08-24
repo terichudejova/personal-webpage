53d48c4ce7dc8db5f11266b8f6f464e08236ac72
Teri Chudějová <tereza.chudejova@gmail.com>
2024-08-25 00:18:29 +0200

    add timeline, add spans

--- src/App.jsx
+++ src/App.jsx
@@ -47,11 +47,13 @@ function App() {
             <div className='firstCard cardDash'></div>
             <div className='secondCard cardDash'></div>
             <div className='thirdCard cardDash'></div>
-            <div className="fourthCard cardDash"><img id='surfPicture' src='images/surf.png'/></div>
+            <div className="fourthCard cardDash">
+              <img id='timeline' src='images/timeline.png'/>
+            </div>
           </div>
           <div className='aboutPopisek'>
             <h3 id='linkOmne'>O mně</h3>
-            <p>Jmenuji se Tereza Chudějová a jsem front-end developerka s IT základy získanými během studia na vysoké škole a vášní pro vytváření moderních, uživatelsky přívětivých webových stránek. Mám bakalářský titul z Aplikované informatiky a inženýrský titul z Informačního managementu, což mi poskytuje technické znalosti a schopnost propojit různé aspekty vývoje, designu a firemních potřeb. Po 5 letech práce jako datový analytik jsem se rozhodla následovat svou vášeň pro kreativní řešení a začala se intenzivně věnovat front-end vývoji, kde mohu spojit logické myšlení s estetikou a designem.</p>
+            <p>Jmenuji se Tereza Chudějová&nbsp;a jsem front-end developerka&nbsp;s IT základy získanými během studia&nbsp;na vysoké škole&nbsp;a vášní pro vytváření moderních, uživatelsky přívětivých webových stránek. Mám bakalářský titul&nbsp;z Aplikované informatiky&nbsp;a inženýrský titul&nbsp;z Informačního managementu, což mi poskytuje technické znalosti&nbsp;a schopnost <span>propojit různé aspekty vývoje, designu&nbsp;a firemních potřeb</span>. Po 5 letech práce jako datový analytik jsem se rozhodla následovat svou vášeň pro kreativní řešení&nbsp;a začala se intenzivně věnovat front-end vývoji, kde mohu <span>spojit logické myšlení&nbsp;s estetikou&nbsp;a designem</span>.</p>
           </div>
           <svg id='splash' width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 400">
             <path d="M300,50 C250,90 150,70 100,150 C50,230 120,280 150,350 C180,420 280,420 320,380 C360,340 460,360 480,300 C500,240 400,220 400,150 C400,80 350,10 300,50 Z" fill="#1698CB" />
