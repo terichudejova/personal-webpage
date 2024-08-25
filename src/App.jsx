f3864704015e3173c0b8d7d9522d9850f6fd898e
Teri Chudějová <tereza.chudejova@gmail.com>
2024-08-25 22:28:09 +0200

    add another timeline version

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import './App.css'
 import RolloutButton from "./RolloutButton"
 import WavyCard from './WavyCard'
 import HomeIcon from '@mui/icons-material/Home';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+import SchoolIcon from '@mui/icons-material/School';
+import CodeIcon from '@mui/icons-material/Code';
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 
 function App() {
@@ -43,17 +49,58 @@ function App() {
       {/* FIRST SECTION - ABOUT ME */}
       <div className='firstSection-aboutMe outerBox'>
         <div className='innerBox'>
-          <div className='aboutPicture'>
-            <div className='firstCard cardDash'></div>
-            <div className='secondCard cardDash'></div>
-            <div className='thirdCard cardDash'></div>
-            <div className="fourthCard cardDash">
-              <img id='timeline' src='images/timeline.png'/>
+          <div className="timeline">
+              <div className='timelineCircle'></div>
+              <div className="timelineLine"></div>
+              <div className="timelineFirst timelineCard">
+                <div id="kpmgIcon" className='iconCircle'><EqualizerIcon className='iconInside'/></div>
+                <h4>2018 - 2023</h4>
+                <p>Datový analytik v KPMG ČR s.r.o.</p>
+              </div>
+              <div className="timelineLine"></div>
+              <div className="timelineSecond timelineCard">
+                <div id="bcIcon" className='iconCircle'><SchoolIcon className='iconInside'/></div>
+                <h4>2021</h4>
+                <p>Bc. - Aplikovaná informatika FIS VŠE</p>
+              </div>
+              <div className="timelineLine"></div>
+              <div className="timelineThird timelineCard">
+                <div id="htmlIcon" className='iconCircle'><CodeIcon className='iconInside'/></div>
+                <h4>10/2023</h4>
+                <p>HTML5/CSS3 kurz</p>
+              </div>
+              <div className="timelineLine"></div>
+              <div className="timelineFourth timelineCard">
+                <div id="jsIcon" className='iconCircle'><JavascriptIcon className='iconInside'/></div>
+                <h4>03/2024</h4>
+                <p>JavaScript kurz</p>
+              </div>
+              <div className="timelineLine"></div>
+              <div className="timelineFifth timelineCard">
+                <div id="ingIcon" className='iconCircle'><SchoolIcon className='iconInside'/></div>
+                <h4>05/2024</h4>
+                <p>Ing. - Informační management FIS VŠE</p>
+              </div>
+              <div className="timelineLine"></div>
+              <div className="timelineSixth timelineCard">
+                <div id="reactIcon" className='iconCircle'><AutoAwesomeIcon className='iconInside'/></div>
+                <h4>06/2024</h4>
+                <p>React kurz</p>
+              </div>
+              <div className="timelineLine"></div>
+              <div className="timelineSeventh timelineCard">
+                <div id="gitIcon" className='iconCircle'><GitHubIcon className='iconInside'/></div>
+                <h4>07/2024</h4>
+                <p>Git & GitHub kurz</p>
+              </div>
+              <div className="timelineLine"></div>
+              <div className='timelineCircle'></div>
             </div>
-          </div>
           <div className='aboutPopisek'>
             <h3 id='linkOmne'>O mně</h3>
             <p>Jmenuji se Tereza Chudějová&nbsp;a jsem front-end developerka&nbsp;s IT základy získanými během studia&nbsp;na vysoké škole&nbsp;a vášní pro vytváření moderních, uživatelsky přívětivých webových stránek. Mám bakalářský titul&nbsp;z Aplikované informatiky&nbsp;a inženýrský titul&nbsp;z Informačního managementu, což mi poskytuje technické znalosti&nbsp;a schopnost <span>propojit různé aspekty vývoje, designu&nbsp;a firemních potřeb</span>. Po 5 letech práce jako datový analytik jsem se rozhodla následovat svou vášeň pro kreativní řešení&nbsp;a začala se intenzivně věnovat front-end vývoji, kde mohu <span>spojit logické myšlení&nbsp;s estetikou&nbsp;a designem</span>.</p>
+            <h4>Záliby</h4>
+            <p></p>
           </div>
           <svg id='splash' width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 400">
             <path d="M300,50 C250,90 150,70 100,150 C50,230 120,280 150,350 C180,420 280,420 320,380 C360,340 460,360 480,300 C500,240 400,220 400,150 C400,80 350,10 300,50 Z" fill="#1698CB" />
@@ -65,7 +112,15 @@ function App() {
 
       {/* SECOND SECTION */}
       <div className="secondSection outerBox">
-
+        <div className='innerBox'>
+          <div className='aboutPicture'>
+            <div className='firstCard cardDash'></div>
+            <div className='secondCard cardDash'></div>
+            <div className='thirdCard cardDash'></div>
+            <div className="fourthCard cardDash"><img id='surfPic' src='images/surf.png'/></div>
+          </div>
+          
+        </div>
       </div>
 
 
